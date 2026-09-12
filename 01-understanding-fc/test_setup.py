f88841e4ab14fd94d6c2e7fdfe9ee506c9b9be4d
Robert Citek <robert.citek@gmail.com>
2026-09-12 15:53:50 +0000

    added #! and chmod +x

--- 01-understanding-fc/test_setup.py
+++ 01-understanding-fc/test_setup.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 from google import genai
 
 client = genai.Client()
--- 02-declaring-functions/declaring_functions.py
+++ 02-declaring-functions/declaring_functions.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 from google import genai
 from google.genai import types
 import requests
